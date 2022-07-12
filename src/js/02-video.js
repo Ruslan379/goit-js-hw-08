d483f90b263d5a6372266e686a7970468819b220
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-12 18:51:30 +0300

    Started task-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,6 @@ import throttle from 'lodash.throttle';
 
 // var throttle = require('lodash.throttle');
 
-
 const iframe = document.querySelector('iframe'); //! Getting Started
 
 // console.log(iframe);
@@ -12,73 +11,54 @@ const iframe = document.querySelector('iframe'); //! Getting Started
 const player = new Player(iframe); //! Getting Started
 
 // TODO ===========================================================
-    // player.on('play', function() {
-    //     console.log('played the video!');
-    // });
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
 
-
-    // player.getVideoTitle().then(function(title) {
-    //     console.log('title:', title);
-    // });
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
 // TODO ===========================================================
 
+//! Работает!
 
-
-    //! Работает!
-
-    //! old ++++++++++++++++++++++++++++++++++++ (без throttle)
+//! old ++++++++++++++++++++++++++++++++++++ (без throttle)
 // player.on('timeupdate', function (timeupdate) {
-    
+
 //         console.log('timeupdate:', timeupdate);
 //         console.log('timeupdate.seconds:', timeupdate.seconds);
 //         console.log('timeupdate.percent:', timeupdate.percent);
 //         console.log('timeupdate.duration:', timeupdate.duration);
-    
-//     const t = timeupdate.seconds
-//     localStorage.setItem("videoplayer-current-time", JSON.stringify(t));
+
+//     // const t = timeupdate.seconds
+//     // localStorage.setItem("videoplayer-current-time", JSON.stringify(t));
+//     localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
 //     console.log(Number(localStorage.getItem("videoplayer-current-time")));
 // });
 //! old ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
 
 // throttle(player.on('timeupdate', timeUpdate), 1000); //! НЕ РАБОТАЕТ!!! (с throttle)
 
 // player.on('timeupdate', timeUpdate); //* РАБОТАЕТ!!! (без throttle)
 
 player.on('timeupdate', throttle(timeUpdate, 1000)); //* РАБОТАЕТ!!! (с throttle)
-    
-    function timeUpdate(timeupdate) {
-    
-        // console.log('timeupdate:', timeupdate);
-        // console.log('timeupdate.seconds:', timeupdate.seconds);
-        // console.log('timeupdate.percent:', timeupdate.percent);
-        // console.log('timeupdate.duration:', timeupdate.duration);
-    
-    const t = timeupdate.seconds;
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(t));
-        
-    console.log(Number(localStorage.getItem("videoplayer-current-time")));
-};
 
+function timeUpdate(timeupdate) {
+  // console.log('timeupdate:', timeupdate);
+  // console.log('timeupdate.seconds:', timeupdate.seconds);
+  // console.log('timeupdate.percent:', timeupdate.percent);
+  // console.log('timeupdate.duration:', timeupdate.duration);
 
+  localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
 
-//! Save to localStorage => currentTime:
-let currentTime = Number(localStorage.getItem("videoplayer-current-time"))
+  //! Консолит currentTimePlayer:
+  console.log(Number(localStorage.getItem('videoplayer-current-time')));
+}
 
+//! Save to currentTimePlayer => localStorage:
+let currentTimePlayer = Number(
+  localStorage.getItem('videoplayer-current-time')
+);
 
-player.setCurrentTime(currentTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
-
+//! возобновляет воспроизведение с сохраненной позиции = currentTimePlayer:
+player.setCurrentTime(currentTimePlayer);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('submit', handleSubmit);
+// console.log(refs.form);
+// console.log(refs.form.elements);
+// console.log(refs.form.elements.email);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  // console.log(event.currentTarget.elements);
+
+  if (email.value === '' || message.value === '')
+    return alert('Все поля должны быть заполнены!');
+
+  const formOut = {};
+  formOut.email = email.value;
+  formOut.message = message.value;
+  console.log(formOut); //! По ТЗ: Вывод в косоль обьекта с введенными данными
+
+  const formData = new FormData(event.currentTarget); //? FormData
+
+  formData.forEach((value, name) => {
+    // console.log('onFormSubmit -> name', name);
+    // console.log('onFormSubmit -> value', value);
+    console.log(`${name}: ${value}`); //! Вывод в косоль с помощью FormData
+  });
+
+  console.log(`Email: ${email.value}, Message: ${message.value}`); //! Вывод в косоль с помощью шаблонной строки
+
+  event.currentTarget.reset();
+}
