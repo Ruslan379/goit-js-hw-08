6b954b53f490d2cf765a6af7ca17ba377f91430a
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-07-12 14:17:16 +0300

    Finished task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,85 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//! подключаем библиотеку simplelightbox в проект:
+
+// Описание в документации:
+import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "E:/GoIT/Code/goit-js-hw-08/node_modules/simplelightbox/dist/simple-lightbox";
+
+// Дополнительный импорт стилей:
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+//! 1.Получаем ссылку на div-контейнер:
+const ImageContainer = document.querySelector('.gallery');
+// console.log(ImageContainer);
+
+//!   2.В переменную ImageMarkup записываем новую разметку 
+//!  как результат вызовыа ф-ции createImageCardsMarkup 
+//!  с аргументом - массив объектов galleryItems):
+const ImageMarkup = createImageCardsMarkup();
+
+//!   3.Добавляем новую разметку в div-контейнер с помощью insertAdjacentHTML:
+ImageContainer.insertAdjacentHTML('beforeend', ImageMarkup);
+//!                            ИЛИ (2-ой вариант):
+//!   3.Добавляем новую разметку в div-контейнер с помощью innerHTML:
+// ImageContainer.innerHTML = ImageMarkup;
+
+
+//!   3.1.Консолим новую разметку div-контейнера:
+// console.log(createImageCardsMarkup(galleryItems));
+
+
+//!   4.Сосдаем слушателя событий методом делегирования:
+// console.log(ImageContainer.addEventListener('click', onImageContainerClick));
+ImageContainer.addEventListener('click', onImageContainerClick);
+// console.log(onImageContainerClick());
+
+
+//!   1.1.Ф-ция, к-рая создает массив с новой разметкой:
+function createImageCardsMarkup() {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </div>
+    `;
+    })
+    .join('');
+}
+
+//!   4.1.Ф-ция, к-рая прослушивает клики от ВСЕХ изображений:
+function onImageContainerClick(evt) {
+    evt.preventDefault(); //TODO отменить действие по умолчанию - preventDefault().
+
+    // console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+    // console.log(evt.target);
+    // console.log(evt.target.src);
+    // console.log(evt.target.dataset.source); //! ссылка на кликнутое оригинальное изображение
+    // console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+
+
+    //! Использование библиотеки SimpleLightbox:
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        // caption: true,
+        captionPosition: 'bottom',  
+        captionDelay: 250,
+        captionsData: "alt",
+    });
+
+    gallery.on('show.simplelightbox', function () {
+    });
+}
+
